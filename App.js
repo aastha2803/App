c9669e720c482c42dfbbad6ea56bf6e1ea624edc
Aastha Mujawdiya <103181989+aastha2803@users.noreply.github.com>
2023-01-13 21:16:49 +0530

    Initial commit

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D9D9D9',
     alignItems: 'center',
     justifyContent: 'center',
   },
